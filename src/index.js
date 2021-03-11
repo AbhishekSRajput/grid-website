53543c763cf2d23a6099700ba2106091d1eb2066
ApcGamer <abhisingh3@hotmail.com>
2021-03-11 22:47:45 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//component
+import App from './App';
+
+//style
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
