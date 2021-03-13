3af1ba0d79663739333e1fcb41a0f508b711e9e9
ApcGamer <abhisingh3@hotmail.com>
2021-03-13 23:23:23 +0530

    positioning grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import GridPlayground from './playground/grid-playground';
+
+//style
+import './App.scss';
+
 const App = () => {
-	return <div>hello</div>;
+	return (
+		<div>
+			<GridPlayground />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/playground/grid-playground.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//style
+import './grid-playground.scss';
+
+const GridPlayground = () => {
+	return (
+		<div className="container">
+			<div className="item item--1">orange</div>
+			<div className="item item--2">green</div>
+			<div className="item item--3">violet</div>
+			<div className="item item--4">pink</div>
+			<div className="item item--5">blue</div>
+			<div className="item item--6">brown</div>
+		</div>
+	);
+};
+
+export default GridPlayground;
