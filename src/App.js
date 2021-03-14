22414c726d6dce99caa0d23485ac4aee477d8db7
ApcGamer <abhisingh3@hotmail.com>
2021-03-15 01:27:34 +0530

    implicit vs expicit grid items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 //import GridPlayground from './playground/grid-playground';
-import ChallengePlayground from './playground/Challenge.playground';
+//import ChallengePlayground from './playground/Challenge.playground';
+import ImplicitVsExplicit from './playground/implicitvsexplicit';
+
 //style
 import './App.scss';
 
 const App = () => {
 	return (
 		<div>
-			<ChallengePlayground />
+			<ImplicitVsExplicit />
 		</div>
 	);
 };
---
+++ src/playground/implicitvsexplicit.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+//style
+import './implicitvsexplicit.scss';
+
+const ImplicitVsExplicit = () => {
+	return (
+		<div className="implicit-container">
+			<div className="item item--1">Modern</div>
+			<div className="item item--2">Css</div>
+			<div className="item item--3">With</div>
+			<div className="item item--4">FlexBox</div>
+			<div className="item item--5">and</div>
+			<div className="item item--6">Grid</div>
+			<div className="item item--7">Is </div>
+			<div className="item item--8">Great</div>
+		</div>
+	);
+};
+
+export default ImplicitVsExplicit;
