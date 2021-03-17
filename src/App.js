b7f5db6b81490407f59e7f79d9b132df9dc31127
ApcGamer <abhisingh3@hotmail.com>
2021-03-17 16:58:25 +0530

    added basic elements and defined grid rows when height is decreasing use vh when width is decreasis use vw

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ import './App.scss';
 
 const App = () => {
 	return (
-		<div>
-			<h1>project setup</h1>
+		<div className="app-container">
+			<div className="sidebar">sidebar</div>
+			<header className="header">header</header>
+			<div className="realtors">top 3 realtors</div>
+			<section className="features">features</section>
+			<div className="story__pictures">story Pictures</div>
+			<div className="story__content">story Content</div>
+			<section className="homes">homes</section>
+			<section className="gallery">gallery</section>
+			<footer className="footer">footer</footer>
 		</div>
 	);
 };
